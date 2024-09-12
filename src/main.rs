7e777b71d0f61eaa8e2f9aa8b33a485316a1a61e
pantaelaman <calvin@theophoffs.com>
2024-09-12 11:58:30 -0400

    Added client command handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use std::ffi::OsStr;
+use core::str;
+use std::{
+  ffi::OsStr,
+  io::Write,
+  process::{Command, Stdio},
+};
 
+use color_eyre::eyre::Result;
 use interprocess::local_socket::{
   tokio::Stream, traits::tokio::Listener, GenericNamespaced, ListenerOptions,
   ToNsName,
 };
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 
 #[tokio::main]
 async fn main() {
@@ -34,6 +41,49 @@ async fn main() {
   }
 }
 
-async fn handle_connection(connection: Stream) -> std::io::Result<()> {
+async fn handle_connection(connection: Stream) -> Result<()> {
+  let mut buffered = BufReader::new(&connection);
+  let mut buffer = Vec::new();
+  buffered.read_until(0x03, &mut buffer).await?;
+  let name = str::from_utf8(&buffer[..buffer.len() - 1])?.to_owned();
+  println!("Starting session {name}");
+  let mut kak = Command::new("kak").args(["-d", "-s", &name]).spawn()?;
+  loop {
+    buffer.clear();
+    buffered.read_until(0x03, &mut buffer).await?;
+    match buffer[0] {
+      0 => {
+        println!("Killing session {name}");
+        kill_session(&name)?;
+        kak.wait()?;
+        break;
+      }
+      1 => {
+        println!("Reloading session {name}");
+        kill_session(&name)?;
+        kak.wait()?;
+        kak = Command::new("kak").args(["-d", "-s", &name]).spawn()?;
+      }
+      0x02 => {
+        println!(
+          "Unrecognised command `{}`",
+          str::from_utf8(&buffer[1..buffer.len() - 1])?
+        );
+      }
+      b => println!("Unrecognised command `{b}`"),
+    }
+  }
+  Ok(())
+}
+
+fn kill_session(session_name: &str) -> std::io::Result<()> {
+  let mut killer = Command::new("kak")
+    .args(["-p", &session_name])
+    .stdin(Stdio::piped())
+    .spawn()?;
+  let mut stdin = killer.stdin.take().unwrap();
+  stdin.write_all(b"kill")?;
+  std::mem::drop(stdin);
+  killer.wait()?;
   Ok(())
 }
